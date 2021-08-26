edecd48aef36bfd234f69bc341a97e7cacb6b1e5
briannlongzhao <briannlongzhao@gmail.com>
2021-08-25 18:38:23 -0700

    strStr() KMP

--- 28-ImplementstrStr()-Easy.cpp
+++ 28-ImplementstrStr()-Easy.cpp
@@ -7,3 +7,54 @@ For the purpose of this problem, we will return 0 when needle is an empty string
 This is consistent to C's strstr() and Java's indexOf().
 */
 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle == "") {
+            return 0;
+        }
+        if (haystack == "") {
+            return -1;
+        }
+        
+        const int len_n = needle.size();
+        vector<int> prefix(len_n);
+        prefix[0] = 0;
+        for (int i = 1, j = 0; i < len_n;) {
+    		if (needle[i] != needle[j]){
+    			if (j > 0){
+    				j = prefix[j-1];
+    			}
+    			else{
+    				i++;
+    			}
+    		}
+    		else {
+    			prefix[i] = j+1;
+    			i++;
+    			j++;
+    		}
+    	}
+        
+        int len_h = haystack.size();
+        int j = 0;
+        for (int i = 0; i < len_h; i++) {
+            if (haystack[i] == needle[j]) {
+                if (j == len_n-1) {
+                    return i-j;
+                }
+                j++;
+            }
+            else {
+                if (j > 0) {
+                    j = prefix[j-1];
+                    i--;
+                }
+                else {
+                    j = 0;
+                }
+            }
+        }
+        return -1;
+    }
+};
